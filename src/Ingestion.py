b63be26297c6aef13265cafa388f8d8d6389d57c
Antonio <antonio331977@hotmail.com>
2025-02-13 17:15:34 +0000

    cambio de ubicacion de archivos

--- src/Ingestion.py
+++ src/Ingestion.py
@@ -3,8 +3,8 @@ import numpy as np
 from tabulate import tabulate
 def carga_datos():
 
-    ordenes= pd.read_csv("../output/Historicos_Ordenes.csv")
-    caracteristicas= pd.read_csv("../output/Caracteristicas_Equipos.csv")
-    registros= pd.read_csv("../output/Registros_Condiciones.csv")
+    ordenes= pd.read_csv("../Historicos_Ordenes.csv")
+    caracteristicas= pd.read_csv("../Caracteristicas_Equipos.csv")
+    registros= pd.read_csv("../Registros_Condiciones.csv")
 
     return ordenes,caracteristicas, registros
